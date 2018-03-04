a906112a96eac4467eabfc9d3a191a33a9a553a6
crisrivlop <crisrivlop@gmail.com>
2018-03-04 14:32:25 -0600

    contador de 2 y 3 bits agregados

--- counter_4_tb.sv
+++ counter_4_tb.sv
@@ -1,11 +1,13 @@
 module counter_4_tb();
 
 	logic clk,reset;
-	logic[3:0] sout;
+	logic[1:0] sout1;
+	logic[2:0] sout2;
+	logic[3:0] sout3;
 	assign reset = 1'b0;
-	counter_parameterizable dut1(clk,reset,sout);
-	counter_parameterizable dut2(clk,reset,sout);
-	counter_parameterizable dut3(clk,reset,sout);
+	counter_parameterizable #(2) dut1(clk,reset,sout1);
+	counter_parameterizable #(3) dut2(clk,reset,sout2);
+	counter_parameterizable #(4) dut3(clk,reset,sout3);
 	
 	always
 		begin
